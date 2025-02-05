55be36544938dab35ba80d0f2cf9fbf8f2b7a772
Arturo López <jhonas29@gmail.com>
2025-02-05 12:25:47 +0100

    SelectPlayer modified => 'levelSelected' magic number fixed. Minor linter changes in Slideshow.tsx & Slideshow.scss.

--- src/components/SelectPlayer.tsx
+++ src/components/SelectPlayer.tsx
@@ -8,95 +8,112 @@ import { capitalizeText } from "../utils/capitalizeText";
 
 type playerDataToShow = "player1" | "player2";
 
-
-function SelectPlayer () {					
-	const [ playerDataToShow, setPlayerDataToShow ] = useState<playerDataToShow>("player1");		// This state changes the player data form displayed, player 1 or player 2
+function SelectPlayer() {
+	const [playerDataToShow, setPlayerDataToShow] =
+		useState<playerDataToShow>("player1"); // This state changes the player data form displayed, player 1 or player 2
 
 	const [, gameStateData] = useGameStateContext();
-	const howManyPlayers = gameStateData.gameState;													// The "howManyPlayers" prop is used to decide whether or not to ask to fill in player 2's data when player 1's data has been filled in. "setGameState" changes App`s GameState
-	const setGameState = gameStateData.setGameState;		
-	
-	let levelSelected = 6;
+	const howManyPlayers = gameStateData.gameState; // The "howManyPlayers" prop is used to decide whether or not to ask to fill in player 2's data when player 1's data has been filled in. "setGameState" changes App`s GameState
+	const setGameState = gameStateData.setGameState;
+
+	const aiDefaultLevel = 6;
+	let levelSelected = aiDefaultLevel;
 
 	let inputNameErrorTimeout: number;
 
-	function handleP1EnterKeyPress (event: React.KeyboardEvent<HTMLInputElement>): void {
+	function handleP1EnterKeyPress(
+		event: React.KeyboardEvent<HTMLInputElement>
+	): void {
 		if (event.key === "Enter") {
 			handlePlayer1OkClick();
 		}
 	}
 
-	function handleP2EnterKeyPress (event: React.KeyboardEvent<HTMLInputElement>): void {
+	function handleP2EnterKeyPress(
+		event: React.KeyboardEvent<HTMLInputElement>
+	): void {
 		if (event.key === "Enter") {
 			handlePlayer2OkClick();
 		}
 	}
 
-	function animateInputOnError () {
-		const player1NameInput = document.getElementById("playerInfoInput") as HTMLInputElement;
+	// Activates an animation in the input field to draw the user's attention when the text entered is invalid.
+	function animateInputOnError() {
+		const player1NameInput = document.getElementById(
+			"playerInfoInput"
+		) as HTMLInputElement;
 		player1NameInput.classList.add(styles.animateInputOnError);
-		inputNameErrorTimeout = window.setTimeout( () => {
+		inputNameErrorTimeout = window.setTimeout(() => {
 			player1NameInput.classList.remove(styles.animateInputOnError);
 		}, 501);
 	}
 
-	function handlePlayer1OkClick () {
-		const player1NameInput = document.getElementById("playerInfoInput") as HTMLInputElement;
-		
+	function handlePlayer1OkClick() {
+		const player1NameInput = document.getElementById(
+			"playerInfoInput"
+		) as HTMLInputElement;
+
 		if (player1NameInput.value) {
-			const player1Name = capitalizeText(player1NameInput.value);										// This function is located in the "utils" folder. Only allow the first letter to be capitalized.
+			const player1Name = capitalizeText(player1NameInput.value); // This function is located in the "utils" folder. Only allow the first letter to be capitalized.
 
 			const player1Portrait = player1Data.portrait;
-			player1Data.changePlayerData(player1Name, player1Portrait);									// 	The "SelectPlayer" component only sets the name of the players in the "PlayersDataContext" context. The child component "Slideshow" is responsible for setting the portraits.
+			player1Data.changePlayerData(player1Name, player1Portrait); // 	The "SelectPlayer" component only sets the name of the players in the "PlayersDataContext" context. The child component "Slideshow" is responsible for setting the portraits.
 
 			aiLevelData.setAiLevel(levelSelected);
 
 			player1NameInput.value = "";
-			
+
 			if (howManyPlayers === "select1Player") {
 				player2Data.changePlayerData("Robotín", "images/portraits/Robot2.jpg");
-				setGameState("gameIntro1P");													// If there is only one player, changes the state of the parent component "App" to start the game
+				setGameState("gameIntro1P"); // If there is only one player, changes the state of the parent component "App" to start the game
 			} else if (howManyPlayers === "select2Players") {
-				setPlayerDataToShow("player2");													// If there are two players, changes the state of this component to ask for player 2´s data.
+				setPlayerDataToShow("player2"); // If there are two players, changes the state of this component to ask for player 2´s data.
 			}
-
 		} else {
 			animateInputOnError();
 		}
 	}
 
-	function handlePlayer2OkClick () {
-		const player2NameInput = document.getElementById("playerInfoInput") as HTMLInputElement;
-		
+	function handlePlayer2OkClick() {
+		const player2NameInput = document.getElementById(
+			"playerInfoInput"
+		) as HTMLInputElement;
+
 		if (player2NameInput.value) {
-			const player2Name = capitalizeText(player2NameInput.value);										// This function is located in the "utils" folder. Only allow the first letter to be capitalized.
+			const player2Name = capitalizeText(player2NameInput.value); // This function is located in the "utils" folder. Only allow the first letter to be capitalized.
 
 			const player2Portrait = player2Data.portrait;
 			player2Data.changePlayerData(player2Name, player2Portrait);
-			
+
 			setGameState("gameIntro2P");
 		} else {
 			animateInputOnError();
 		}
 	}
 
-
-	function handleDifficultyClick (event: React.MouseEvent<HTMLParagraphElement> ) {
+	function handleDifficultyClick(
+		event: React.MouseEvent<HTMLParagraphElement>
+	) {
 		const selectedDifficultyId = (event.target as HTMLParagraphElement).id;
-		const containerToHighlight = (event.target as HTMLParagraphElement).parentElement as HTMLDivElement;
+		const containerToHighlight = (event.target as HTMLParagraphElement)
+			.parentElement as HTMLDivElement;
 		levelSelected = Number(selectedDifficultyId);
 
 		removeHighlightedDifficultStyles();
 		containerToHighlight.classList.add(styles.difficultySelected);
-		
 	}
 
-	function removeHighlightedDifficultStyles () {
-		const removeHighlightedDifficultStyles = document.querySelector(`.${styles.difficultySelected}`);								
-		if (removeHighlightedDifficultStyles) removeHighlightedDifficultStyles.classList.remove(styles.difficultySelected);
+	function removeHighlightedDifficultStyles() {
+		const removeHighlightedDifficultStyles = document.querySelector(
+			`.${styles.difficultySelected}`
+		);
+		if (removeHighlightedDifficultStyles)
+			removeHighlightedDifficultStyles.classList.remove(
+				styles.difficultySelected
+			);
 	}
 
-	useEffect( () => {
+	useEffect(() => {
 		return () => {
 			clearTimeout(inputNameErrorTimeout);
 		};
@@ -104,69 +121,118 @@ function SelectPlayer () {
 
 	if (playerDataToShow === "player1" && isAIGameData.isAIGame) {
 		return (
-			<div className={styles.mainContainer} >
-				<div className={`${styles.playerInfoContainer} ${styles.aiGamePlayerInfoContainer}`} >
-					<div className={styles.playerNumberAndPieceContainer} >
-						<p className={styles.playerInfoNumber} >Jugador 1</p>
-						<img alt="White king" src="images/pieces/kingW.png" ></img>
+			<div className={styles.mainContainer}>
+				<div
+					className={`${styles.playerInfoContainer} ${styles.aiGamePlayerInfoContainer}`}
+				>
+					<div className={styles.playerNumberAndPieceContainer}>
+						<p className={styles.playerInfoNumber}>Jugador 1</p>
+						<img alt="White king" src="images/pieces/kingW.png"></img>
 					</div>
 
-					<div className={styles.slideshowContainer} >
-						<Slideshow  player="p1"/ >
+					<div className={styles.slideshowContainer}>
+						<Slideshow player="p1" />
 					</div>
 
 					<div className={styles.difficultLevelsContainer}>
 						<div className={styles.difficultyContainer}>
-							<p  id="1" onClick={handleDifficultyClick}>Fácil</p>
+							<p id="1" onClick={handleDifficultyClick}>
+								Fácil
+							</p>
 						</div>
 
-						<div className={`${styles.difficultyContainer} ${styles.difficultySelected}`}>
-							<p id="6" onClick={handleDifficultyClick}>Normal</p>
+						<div
+							className={`${styles.difficultyContainer} ${styles.difficultySelected}`}
+						>
+							<p id="6" onClick={handleDifficultyClick}>
+								Normal
+							</p>
 						</div>
 
 						<div className={styles.difficultyContainer}>
-							<p id="13" onClick={handleDifficultyClick}>Difícil</p>
+							<p id="13" onClick={handleDifficultyClick}>
+								Difícil
+							</p>
 						</div>
 					</div>
 
-					<input className={styles.playerInfoInput} maxLength={9} id="playerInfoInput" placeholder="Tu nombre" onKeyDown={handleP1EnterKeyPress} autoComplete="off"></input>
-					<button type="button" className={styles.playerInfobtnOk} onClick={handlePlayer1OkClick}>Ok!</button>
+					<input
+						className={styles.playerInfoInput}
+						maxLength={9}
+						id="playerInfoInput"
+						placeholder="Tu nombre"
+						onKeyDown={handleP1EnterKeyPress}
+						autoComplete="off"
+					></input>
+					<button
+						type="button"
+						className={styles.playerInfobtnOk}
+						onClick={handlePlayer1OkClick}
+					>
+						Ok!
+					</button>
 				</div>
 			</div>
 		);
-	} else if (playerDataToShow === "player1" && !isAIGameData.isAIGame){
+	} else if (playerDataToShow === "player1" && !isAIGameData.isAIGame) {
 		return (
-			<div className={styles.mainContainer} >
-				<div className={styles.playerInfoContainer} >
-					<div className={styles.playerNumberAndPieceContainer} >
-						<p className={styles.playerInfoNumber} >Jugador 1</p>
-						<img alt="White king" src="images/pieces/kingW.png" ></img>
+			<div className={styles.mainContainer}>
+				<div className={styles.playerInfoContainer}>
+					<div className={styles.playerNumberAndPieceContainer}>
+						<p className={styles.playerInfoNumber}>Jugador 1</p>
+						<img alt="White king" src="images/pieces/kingW.png"></img>
 					</div>
 
-					<div className={styles.slideshowContainer} >
-						<Slideshow  player="p1"/ >
+					<div className={styles.slideshowContainer}>
+						<Slideshow player="p1" />
 					</div>
 
-					<input className={styles.playerInfoInput} maxLength={9} id="playerInfoInput" placeholder="Tu nombre" onKeyDown={handleP1EnterKeyPress} autoComplete="off"></input>
-					<button type="button" className={styles.playerInfobtnOk} onClick={handlePlayer1OkClick}>Ok!</button>
+					<input
+						className={styles.playerInfoInput}
+						maxLength={9}
+						id="playerInfoInput"
+						placeholder="Tu nombre"
+						onKeyDown={handleP1EnterKeyPress}
+						autoComplete="off"
+					></input>
+					<button
+						type="button"
+						className={styles.playerInfobtnOk}
+						onClick={handlePlayer1OkClick}
+					>
+						Ok!
+					</button>
 				</div>
 			</div>
 		);
 	} else {
 		return (
-			<div className={styles.mainContainer} >
-				<div className={styles.playerInfoContainer} >
+			<div className={styles.mainContainer}>
+				<div className={styles.playerInfoContainer}>
 					<div className={styles.playerNumberAndPieceContainer}>
-						<p className={styles.playerInfoNumber} >Jugador 2</p>
+						<p className={styles.playerInfoNumber}>Jugador 2</p>
 						<img alt="Black king" src="images/pieces/kingB.png"></img>
 					</div>
 
-					<div className={styles.slideshowContainer} >
-						<Slideshow  player="p2"/ >
+					<div className={styles.slideshowContainer}>
+						<Slideshow player="p2" />
 					</div>
 
-					<input className={styles.playerInfoInput} maxLength={9} id="playerInfoInput" placeholder="Tu nombre" onKeyDown={handleP2EnterKeyPress} autoComplete="off"></input>
-					<button type="button" className={styles.playerInfobtnOk} onClick={handlePlayer2OkClick} >Ok!</button>
+					<input
+						className={styles.playerInfoInput}
+						maxLength={9}
+						id="playerInfoInput"
+						placeholder="Tu nombre"
+						onKeyDown={handleP2EnterKeyPress}
+						autoComplete="off"
+					></input>
+					<button
+						type="button"
+						className={styles.playerInfobtnOk}
+						onClick={handlePlayer2OkClick}
+					>
+						Ok!
+					</button>
 				</div>
 			</div>
 		);
--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -4,63 +4,73 @@ import { useEffect, useState } from "react";
 import { player1Data, player2Data } from "../globals/playersData";
 
 type SlideshowProps = {
-	player: "p1" |"p2"
-}
+	player: "p1" | "p2";
+};
 
-function Slideshow( {player}: SlideshowProps ) {							//	The "player" prop indicates which player is selecting their portrait.
-	const [ actualPortraitIndex, setActualPortraitIndex ] = useState(0);
+function Slideshow({ player }: SlideshowProps) {
+	//	The "player" prop indicates which player is selecting their portrait.
+	const [actualPortraitIndex, setActualPortraitIndex] = useState(0);
 	let actualPortraitPath: string;
-	
-	let noSelectedPortraitsPaths = portraitsPaths; 			// Later, this variable will be defined as "portraitsPaths" minus the portrait already selected by player 1, if any
-	
-	let prevPortraitPath: string;		// 	These variables will be defined later, within "if" statements.
-	let nextPortraitPath: string;		
-	let nextPortraitTimeout: number;	
+
+	let noSelectedPortraitsPaths = portraitsPaths; // Later, this variable will be defined as "portraitsPaths" minus the portrait already selected by player 1, if any
+
+	let prevPortraitPath: string; // 	These variables will be defined later, within "if" statements.
+	let nextPortraitPath: string;
+	let nextPortraitTimeout: number;
 	let prevPortraitTimeout: number;
 
-
-	if ( player === "p2" ) {							// If player 2 (p2) is selecting his / her portrait, the portrait already selected by player 1 (playersData[0].portrait) will be removed from the array of available portraits for player 2.
+	if (player === "p2") {
+		// If player 2 (p2) is selecting his / her portrait, the portrait already selected by player 1 (playersData[0].portrait) will be removed from the array of available portraits for player 2.
 		const noSelectedPortraitsPathsCopy = [...noSelectedPortraitsPaths];
 
-		const pathToDeleteIndex = noSelectedPortraitsPathsCopy.findIndex( (element) => {
-			return element === player1Data.portrait;
-		});
+		const pathToDeleteIndex = noSelectedPortraitsPathsCopy.findIndex(
+			(element) => {
+				return element === player1Data.portrait;
+			}
+		);
 
 		noSelectedPortraitsPathsCopy.splice(pathToDeleteIndex, 1);
 		noSelectedPortraitsPaths = noSelectedPortraitsPathsCopy;
-	} 
+	}
 
-	if ( actualPortraitIndex > noSelectedPortraitsPaths.length - 1) {				// 		If player 1 chose the last portrait from the "noSelectedPortraitsPaths" array, "currentPortraitIndex" will be left with a value greater than the length of the array, causing errors for player 2. This code corrects this situation.
+	if (actualPortraitIndex > noSelectedPortraitsPaths.length - 1) {
+		// 		If player 1 chose the last portrait from the "noSelectedPortraitsPaths" array, "currentPortraitIndex" will be left with a value greater than the length of the array, causing errors for player 2. This code corrects this situation.
 		actualPortraitPath = noSelectedPortraitsPaths[0];
 		setActualPortraitIndex(0);
 	} else {
 		actualPortraitPath = noSelectedPortraitsPaths[actualPortraitIndex];
 	}
 
-	if (player === "p1") {												// Every time a portrait is chosen (the one that appears by default also counts) it is set as the one chosen by player 1 or 2 in the PlayersDataContext context
+	if (player === "p1") {
+		// Every time a portrait is chosen (the one that appears by default also counts) it is set as the one chosen by player 1 or 2 in the PlayersDataContext context
 		player1Data.changePlayerData("", actualPortraitPath);
 	} else {
 		player2Data.changePlayerData("", actualPortraitPath);
 	}
 
-	if (actualPortraitIndex < (noSelectedPortraitsPaths.length - 1)) {						// 	Defines the initial portrait that contains the nextPortraitPath variable, based on the portrait that contains the currentPortraitIndex variable
+	if (actualPortraitIndex < noSelectedPortraitsPaths.length - 1) {
+		// 	Defines the initial portrait that contains the nextPortraitPath variable, based on the portrait that contains the currentPortraitIndex variable
 		nextPortraitPath = noSelectedPortraitsPaths[actualPortraitIndex + 1];
 	} else {
 		nextPortraitPath = noSelectedPortraitsPaths[0];
 	}
 
-	if (actualPortraitIndex > 0) {															// 	Defines the initial portrait that contains the prevPortraitPath variable, based on the portrait that contains the currentPortraitIndex variable
+	if (actualPortraitIndex > 0) {
+		// 	Defines the initial portrait that contains the prevPortraitPath variable, based on the portrait that contains the currentPortraitIndex variable
 		prevPortraitPath = noSelectedPortraitsPaths[actualPortraitIndex - 1];
 	} else {
-		prevPortraitPath = noSelectedPortraitsPaths[noSelectedPortraitsPaths.length - 1];
+		prevPortraitPath =
+			noSelectedPortraitsPaths[noSelectedPortraitsPaths.length - 1];
 	}
 
-	function handleNextPortrait () {
-		const nextImageContainer = document.getElementById("nextImageContainer") as HTMLDivElement;
+	function handleNextPortrait() {
+		const nextImageContainer = document.getElementById(
+			"nextImageContainer"
+		) as HTMLDivElement;
 		nextImageContainer.classList.add(styles.animateNextPortrait);
 
-		nextPortraitTimeout = window.setTimeout( () => {
-			if (actualPortraitIndex < (noSelectedPortraitsPaths.length - 1)) {
+		nextPortraitTimeout = window.setTimeout(() => {
+			if (actualPortraitIndex < noSelectedPortraitsPaths.length - 1) {
 				setActualPortraitIndex(actualPortraitIndex + 1);
 			} else {
 				setActualPortraitIndex(0);
@@ -70,11 +80,13 @@ function Slideshow( {player}: SlideshowProps ) {							//	The "player" prop indi
 		}, 400);
 	}
 
-	function handlePrevPortrait () {
-		const prevImageContainer = document.getElementById("prevImageContainer") as HTMLDivElement;
+	function handlePrevPortrait() {
+		const prevImageContainer = document.getElementById(
+			"prevImageContainer"
+		) as HTMLDivElement;
 		prevImageContainer.classList.add(styles.animatePrevPortrait);
 
-		prevPortraitTimeout = window.setTimeout( () => {
+		prevPortraitTimeout = window.setTimeout(() => {
 			if (actualPortraitIndex > 0) {
 				setActualPortraitIndex(actualPortraitIndex - 1);
 			} else {
@@ -85,7 +97,7 @@ function Slideshow( {player}: SlideshowProps ) {							//	The "player" prop indi
 		}, 400);
 	}
 
-	useEffect( () => {
+	useEffect(() => {
 		return () => {
 			clearTimeout(nextPortraitTimeout);
 			clearTimeout(prevPortraitTimeout);
@@ -107,11 +119,11 @@ function Slideshow( {player}: SlideshowProps ) {							//	The "player" prop indi
 					<img alt="Portrait" src={nextPortraitPath} />
 				</div>
 
-				<div className={styles.nextIconContainer} onClick={handleNextPortrait} >
+				<div className={styles.nextIconContainer} onClick={handleNextPortrait}>
 					<img alt="Next portrait" src="icons/icon-next.svg" />
 				</div>
 
-				<div className={styles.prevIconContainer} onClick={handlePrevPortrait} >
+				<div className={styles.prevIconContainer} onClick={handlePrevPortrait}>
 					<img alt="Previous portrait" src="icons/icon-previous.svg" />
 				</div>
 			</div>
